74d0fdade3359c8f062f1a224e5ff6583eee4d5e
Daniel Alvarez <daniel.yllanes@hotmail.com>
2025-01-20 15:40:26 -0500

    completed

--- algorithm.js
+++ algorithm.js
@@ -1,5 +1,13 @@
 function x(text,char){
-    return 'test'
+    let answer = ''
+    if (text == '' || char == '') {
+        return ''
+    }else{
+        for (let i=0;i<text.length;i++){
+            answer = answer.concat(char)
+        }
+    }
+    return answer
 }
 
 console.log(`x('123','') -> '' / `, x('123', ''))
